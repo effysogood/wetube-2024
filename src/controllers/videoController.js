8188c640a66f71f9537e5b779008fb0dd628fbfa
Saeyoung Choi <effysogood@gmail.com>
2023-10-09 20:41:26 +0900

    feat: backup views and mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,51 +1,42 @@
-const fakeUser = {
-    username: 'Effy',
-    loggedIn: false,
-};
-
+const videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    commemts: 2,
+    createdAt: '2 minutes ago',
+    views: 1,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 4,
+    commemts: 2,
+    createdAt: '20 minutes ago',
+    views: 40,
+    id: 2,
+  },
+  {
+    title: 'Third Video',
+    rating: 3,
+    commemts: 2,
+    createdAt: '1 hour ago',
+    views: 555,
+    id: 3,
+  },
+];
 const trending = (req, res) => {
-    const videos = [
-        {
-            title: 'First Video',
-            rating: 5,
-            commemts: 2,
-            createdAt: '2 minutes ago',
-            views: 45,
-            id: 1,
-        },
-        {
-            title: 'Second Video',
-            rating: 4,
-            commemts: 2,
-            createdAt: '20 minutes ago',
-            views: 40,
-            id: 2,
-        },
-        {
-            title: 'Third Video',
-            rating: 3,
-            commemts: 2,
-            createdAt: '1 hour ago',
-            views: 555,
-            id: 3,
-        },
-    ];
-    return res.render('home', {
-        pageTitle: 'Home',
-        fakeUser: fakeUser,
-        videos,
-    });
+  return res.render('home', { pageTitle: 'Home', videos });
 };
+const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
+};
+const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('edit', { pageTitle: `Editing: ${video.title}`, video });
+};
+const postEdit = (req, res) => {};
 
-const see = (req, res) => res.render('watch', { pageTitle: 'Watch Video' });
-const search = (req, res) => res.render('edit', { pageTitle: 'Edit Video' });
-const edit = (req, res) => {
-    console.log(req.params);
-    return res.send('Edit Video');
-};
-const upload = (req, res) => res.send('Upload Video');
-const remove = (req, res) => {
-    return res.send('Remove Video');
-};
-
-export { trending, upload, search, see, edit, remove };
+export { trending, watch, getEdit, postEdit };
